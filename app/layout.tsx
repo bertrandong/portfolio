40d0c500397657ba83579ddf7a8a6d7345571ec5
bertrandong <bertrandongchangheng@gmail.com>
2026-06-04 22:01:02 +0900

    feat: add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Press_Start_2P } from "next/font/google";
+import Script from "next/script";
 import { ThemeProvider } from "@/lib/ThemeContext";
 import "./globals.css";
 
@@ -38,6 +39,20 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} ${pressStart2P.variable} h-screen antialiased`}
     >
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F1P8V7VTWY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F1P8V7VTWY');
+          `}
+        </Script>
+      </head>
       <body className="h-screen overflow-hidden">
         <ThemeProvider>{children}</ThemeProvider>
       </body>
